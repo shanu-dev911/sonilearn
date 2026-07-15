136fdfb1643579806e14651ce0381065a0001dc9
shanu-dev911 <shanusharma7371@gmail.com>
2026-07-15 19:55:24 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 1. Typescript aur Linting ke saare static check ko skip karein
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // 2. Next.js compiler ko strictly bolna ki data collection bypass kare
+  images: {
+    unoptimized: true
+  }
+};
+
+export default nextConfig;
