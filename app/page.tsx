aba97494414691f2974500059148a593ff158338
Shanu8908900 <sonukumark4646@gmail.com>
2026-08-30 17:51:51 +0100

    Fix character image path to match uploaded filename

--- app/page.tsx
+++ app/page.tsx
@@ -323,7 +323,7 @@ export default function Dashboard() {
 
             <div className="hidden sm:block w-36 md:w-44 flex-shrink-0 -mb-8">
               <Image
-                src="/character.png"
+                src="/ram.png"
                 alt="Study Buddy"
                 width={200}
                 height={260}
