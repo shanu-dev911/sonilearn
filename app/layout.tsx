e4c7fa52988e3da1ddba2c92eacb6a5471d4e6d1
Shanu8908900 <sonukumark4646@gmail.com>
2026-09-10 19:21:27 +0100

    use SoniLearn logo as application favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,6 +87,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/logo.svg" />
       </head>
       <body>
         <FirebaseProvider>
